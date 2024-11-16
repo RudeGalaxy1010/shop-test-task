96ff36bb37f4a348a956145577e1ee8bf8b65c66
RudeGalaxy1010 <Karev007@bk.ru>
2024-11-16 18:20:02 +0300

    Add prefabs usage on start

--- Assets/Source/Infrastructure/Bootstrap.cs
+++ Assets/Source/Infrastructure/Bootstrap.cs
@@ -12,28 +12,38 @@ using UnityEngine;
 namespace Source.Infrastructure {
     public class Bootstrap : MonoBehaviour {
         [SerializeField] private Transform _canvasTransform;
-        [SerializeField] private ShopItemView shopItemViewPrefab;
-        [SerializeField] private PacksShopView shopView;
         [SerializeField] private AllPacksData _allPacksData;
-        [SerializeField] private ExceptionPopup _exceptionPopup;
-        [SerializeField] private UserBalanceView _userBalanceView;
+        [SerializeField] private PacksShopView _shopViewPrefab;
+        [SerializeField] private UserBalanceView _userBalanceViewPrefab;
+        [SerializeField] private ExceptionPopup _exceptionPopupPrefab;
 
         [Header("Test values")]
         [SerializeField] private int _startBalance;
         [SerializeField] private List<ShopPackData> _purchasedPacks;
 
         private void Start() {
+            // Create scene objects
+            IShopView shopView = Instantiate(_shopViewPrefab, _canvasTransform);
+            UserBalanceView userBalanceView = Instantiate(_userBalanceViewPrefab, _canvasTransform);
+            ExceptionPopup exceptionPopup = Instantiate(_exceptionPopupPrefab, _canvasTransform);
+            
+            // Setup user data
             List<string> purchasedItemsIds = _purchasedPacks?.Where(x => x != null).Select(x => x.Id).ToList();
             IUserDataService userDataService = new UserDataService(_startBalance, purchasedItemsIds);
+            
+            // Setup shop
             IShopPacksProvider shopPacksProvider = new DirectLinkShopPacksProvider(_allPacksData);
             IShopModel shopModel = new PacksShopModel(shopPacksProvider);
             
             shopView.Init(shopModel);
 
-            IShopController shopController =
-                new PacksShopController(userDataService, shopModel, shopView, _exceptionPopup);
+            IShopController shopController = new PacksShopController(
+                userDataService, 
+                shopModel, 
+                shopView, 
+                exceptionPopup);
             
-            _userBalanceView.Init(userDataService);
+            userBalanceView.Init(userDataService);
         }
     }
 }
