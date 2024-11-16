c19382e747221c471f9cf713c4d28107f8cb9e4f
RudeGalaxy1010 <Karev007@bk.ru>
2024-11-16 18:15:15 +0300

    Clean up

--- Assets/Source/Pool/IPool.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-namespace Source.Pool {
-    public interface IPool<T> where T : MonoBehaviour {
-        T Get();
-        void Release(T obj);
-    }
-}
--- Assets/Source/Shop/View/ShopItemView.cs
+++ Assets/Source/Shop/View/ShopItemView.cs
@@ -5,15 +5,9 @@ using UnityEngine.UI;
 
 namespace Source.Shop.View {
     public class ShopItemView : MonoBehaviour {
-        public struct ViewData {
-            public Sprite Icon;
-            public int Amount;
-        }
-        
         [SerializeField] private Image _icon;
         [SerializeField] private TMP_Text _value;
-
-        // TODO: Fallback icon
+        
         public void Assign(ShopItemData item, int quantity) {
             if (item == null) {
                 gameObject.SetActive(false);
